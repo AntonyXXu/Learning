4817c77aa1cf47e49a1813962d2b0fd8d29a4246
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-19 10:21:36 -0600

    C# learning

---
+++ C#/CLI day1 project/CLI day1 project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLI_day1_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
