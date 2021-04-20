02b264a6d86ed3fe284b7bb5eb6924f4db8d544f
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-20 11:12:41 -0600

    Add Program.cs, C#

--- C#/day2_exercises/day2Exercises/Program.cs
+++ C#/day2_exercises/day2Exercises/Program.cs
@@ -18,6 +18,39 @@ namespace day2Exercises
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            string customerType = "R";
+            int subtotal = 100;
+            decimal discountPercent;
+
+            switch (customerType)
+            {
+                case "R":
+                    if (subtotal < 100)
+                    {
+                        discountPercent = .0m;
+                    }
+                    else if (subtotal >= 100 && subtotal < 250)
+                    {
+                        discountPercent = .1m;
+                    }
+                    else
+                    {
+                        discountPercent = .25m;
+                    }
+                    break;
+                case "C":
+
+                    if (subtotal < 250)
+                    { discountPercent = .2m; }
+                    else
+                    {
+                        discountPercent = .3m;
+                    }
+                    break;
+                default:
+                    discountPercent = .4m;
+                    break;
+            }
         }
     }
 }
