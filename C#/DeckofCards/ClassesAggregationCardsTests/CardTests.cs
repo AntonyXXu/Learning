9cf390e01ff0e260b1da0da124d0d30c698140a5
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-26 09:24:00 -0600

    unit testing practice

---
+++ C#/DeckofCards/ClassesAggregationCardsTests/CardTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassesAggregationCards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAggregationCards.Tests
+{
+    [TestClass()]
+    public class CardTests
+    {
+        [TestMethod()]
+        public void CardTest()
+        {
+            Card crd;
+            Rank rank = (Rank)1;
+            Suit suit = (Suit)4;
+            crd = new Card(suit, rank);
+            Assert.IsNotNull(crd);
+        }
+    }
+}
