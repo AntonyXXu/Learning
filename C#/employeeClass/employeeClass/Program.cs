120268f02d2315e5f03ebf30fef46002090b215c
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-27 09:41:21 -0600

    Added employeeclass c#

---
+++ C#/employeeClass/employeeClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace employeeClass
+{
+    public class Employee
+    {
+       public Employee(string n, int time, decimal pay)
+        {
+            name = n;
+            hour = time;
+            rate = pay;
+        }
+        public string name { get; set; }
+        public int hour { get; set; }
+        public decimal rate { get; set; }
+        public decimal calcPay()
+        {
+            return hour * rate;
+        }
+        public override string ToString()
+        {
+            return name + " worked " + hour + " at " + rate.ToString("C");
+        }
+    }
+
+    public class PermanentEmployee : Employee
+    {
+        public PermanentEmployee(string n, int time, decimal pay, decimal rrsp) :
+            base(n, time, pay)
+        {
+            rrspPercent = rrsp;
+        }
+        public decimal rrspPercent { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PermanentEmployee john = new PermanentEmployee("john", 40, 30.0m, 0.1m);
+            Console.WriteLine(john);
+            Console.WriteLine(john.calcPay());
+            Console.WriteLine(john.rrspPercent);
+        }
+    }
+}
