6c89b5934b793710dea881a6d4d4af2ccbb7a2d5
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-28 08:45:05 -0600

    added unit test

---
+++ C#/ElectricityBillApplication/CustomerLibraryTests/CustomerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElectricityBillApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityBillApplication.Tests
+{
+    [TestClass()]
+    public class CustomerTests
+    {
+        [TestMethod()]
+        public void CustomerTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void calculateChargeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+
+        }
+    }
+}
