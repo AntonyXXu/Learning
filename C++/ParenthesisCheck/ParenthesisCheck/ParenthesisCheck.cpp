855262e09eb2ad0169db65e90cc868a8d912f45a
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-29 15:09:13 -0600

    parenthesis C++

---
+++ C++/ParenthesisCheck/ParenthesisCheck/ParenthesisCheck.cpp
@@ -0,0 +1,45 @@
+#include <stack>
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool isValid(string parenthesis) {
+  stack<char> stk;
+  string open = "({[";
+  string close = ")}]";
+  for (int i = 0; i < parenthesis.size(); i++)
+  {
+    char p = parenthesis[i];
+    if (open.find(p) < close.size()) {
+      stk.push(p);
+    }
+    else if (close.find(p)<close.size()) {
+      int top = open.find(stk.top());
+      stk.pop();
+      int closeChar = close.find(p);
+      if (top != closeChar) {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+  }
+  return true;
+}
+
+  int main()
+  {
+    cout << isValid("[{()}[]]") << endl;
+  }
+
+  // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+  // Debug program: F5 or Debug > Start Debugging menu
+
+  // Tips for Getting Started: 
+  //   1. Use the Solution Explorer window to add/manage files
+  //   2. Use the Team Explorer window to connect to source control
+  //   3. Use the Output window to see build output and other messages
+  //   4. Use the Error List window to view errors
+  //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+  //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
