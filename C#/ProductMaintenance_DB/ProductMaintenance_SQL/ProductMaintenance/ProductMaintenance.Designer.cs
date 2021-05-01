1668a77da5b7ca3fcc42e903fceaf6cfa69a55c9
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-30 19:15:43 -0600

    connected to DB, rendered results

---
+++ C#/ProductMaintenance_DB/ProductMaintenance_SQL/ProductMaintenance/ProductMaintenance.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ProductMaintenance
+{
+    partial class frmProductMaintenance
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxProducts = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxProducts
+            // 
+            this.lstbxProducts.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstbxProducts.FormattingEnabled = true;
+            this.lstbxProducts.ItemHeight = 15;
+            this.lstbxProducts.Location = new System.Drawing.Point(36, 61);
+            this.lstbxProducts.Name = "lstbxProducts";
+            this.lstbxProducts.Size = new System.Drawing.Size(708, 259);
+            this.lstbxProducts.TabIndex = 0;
+            this.lstbxProducts.SelectedIndexChanged += new System.EventHandler(this.lstbxProducts_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(36, 342);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 56);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(166, 342);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(115, 56);
+            this.btnModify.TabIndex = 1;
+            this.btnModify.Text = "&Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(298, 342);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(115, 56);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(629, 342);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(115, 56);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmProductMaintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 434);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstbxProducts);
+            this.Name = "frmProductMaintenance";
+            this.Text = "Product Maintenance";
+            this.Load += new System.EventHandler(this.frmProductMaintenance_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxProducts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ C#/ProductMaintenance_DB/ProductMaintenance_SQL/ProductMaintenance/ProductMaintenance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProductMaintenance.Models;
+
+namespace ProductMaintenance
+{
+    public partial class frmProductMaintenance : Form
+    {
+        TechSupportContext contextTS;
+        Product currProduct;
+
+        public frmProductMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        private void frmProductMaintenance_Load(object sender, EventArgs e)
+        {
+            contextTS = new TechSupportContext();
+            currProduct = contextTS.Products.First();   //Select the first customer
+            lstbxProducts.DataSource = contextTS.Products.ToList();
+
+        }
+
+        private void lstbxProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
