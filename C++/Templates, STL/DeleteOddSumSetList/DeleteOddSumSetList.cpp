f52cc21b31313a13f40ffb5b5052c6856c921f61
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-03 13:12:01 -0600

    Iterator Delete Set Cpp

--- C++/Templates, STL/DeleteOddSumSetList/DeleteOddSumSetList.cpp
+++ C++/Templates, STL/DeleteOddSumSetList/DeleteOddSumSetList.cpp
@@ -1,12 +1,54 @@
 // DeleteOddSumSetList.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// With a set of pointers to a list of integers, delete all the odd-summed lists and return the number of lists
+// that are removed.
 
 #include <iostream>
+#include <set>
+#include <list>
+using namespace std;
 
+int deleteOddSumLists(set <list <int>*>& st) {
+  set<list<int>*>::iterator setItr;
+  setItr = st.begin();
+  int numDeleted = 0;
+  while (setItr != st.end()) {
+    list<int>::iterator listItr;
+    listItr = (*setItr)->begin();
+    int sum = 0;
+    while (listItr != (*setItr)->end()){
+      sum += (*listItr);
+      listItr++;
+  }
+    if (sum % 2 != 0) {
+      setItr = st.erase(setItr);
+      numDeleted++;
+    }
+    else {
+      setItr++;
+    }    
+  }
+  return numDeleted;
+}
+
+// Sample driver code:
 int main()
 {
-    std::cout << "Hello World!\n";
+  set<list<int>*> s;
+  list<int>* l1 = new list<int>;
+  l1->push_back(1);
+  l1->push_back(2);
+  list<int>* l2 = new list<int>;;
+  l2->push_back(1);
+  l2->push_back(1);
+  list<int>* l3 = new list<int>;;
+  l3->push_back(1);
+  l3->push_back(0);
+  s.insert(l1);
+  s.insert(l2);
+  s.insert(l3);
+  cout << deleteOddSumLists(s) << endl;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
