e0859ffefcbc538c911d42170e39612635bcc7af
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-03 13:21:43 -0600

    removeZeroesSTL

---
+++ C++/Templates, STL/STLListVector_RemoveAllZeros/STLListVector_RemoveAllZeros.cpp
@@ -0,0 +1,47 @@
+// STLListVector_RemoveAllZeros.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+void removeAllZeroes(list<int>& lst) {
+  list<int>::iterator itr = lst.begin();
+  while (itr != lst.end()) {
+    if ((*itr) == 0) {
+      itr = lst.erase(itr);
+    }
+    else {
+      itr++;
+    }
+  }
+}
+
+void removeAllZeroes(vector<int>& lst) {
+  vector<int>::iterator itr = lst.begin();
+  while (itr != lst.end()) {
+    if ((*itr) == 0) {
+      itr = lst.erase(itr);
+    }
+    else {
+      itr++;
+    }
+  }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
