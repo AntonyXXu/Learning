4484927331751e55d12cb5830fca49fd3fbbd58b
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-03 21:35:57 -0600

    Created Stack with template cpp

--- C++/Templates, STL/StackTemplate/StackTemplate.cpp
+++ C++/Templates, STL/StackTemplate/StackTemplate.cpp
@@ -1,9 +1,65 @@
 // StackTemplate.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // 
-// Build a Template Stack class using a linked list
+// Build a Template Stack class using a linked list with push, pop, top, empty
 //
 
 #include <iostream>
+using namespace std;
+
+template <typename T>
+class Stack {
+public:
+  Stack();
+  void push(T data);
+  void pop();
+  T top();
+  bool isEmpty();
+  ~Stack();
+private:
+  struct Node {
+    T data;
+    Node* next;
+  };
+  Node* m_head;
+};
+template <typename T>
+Stack<T>::Stack() {
+  m_head = nullptr;
+}
+template <typename T>
+Stack<T>::~Stack() {
+  while (m_head != nullptr) {
+    Node* del = m_head;
+    m_head = m_head->next;
+    delete del;
+  }
+}
+template <typename T>
+void Stack<T>::push(T data) {
+  Node newNode = new Node();
+  newNode.data = data;
+  newNode.next = m_head;
+  m_head = newNode;  
+}
+template <typename T>
+void Stack<T>::pop() {
+  Node* del = m_head;
+  m_head = del->next;
+  delete del;
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() {
+  return m_head == nullptr;
+}
+
+template <typename T>
+T Stack<T>::top() {
+  if (m_head != nullptr) {
+    return m_head->val;
+  }
+  return T();
+}
 
 int main()
 {
