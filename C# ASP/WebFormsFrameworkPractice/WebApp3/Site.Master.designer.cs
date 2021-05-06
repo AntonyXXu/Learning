e05ae025e248b44b5231ed1ab31dd0433806007f
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-06 11:09:31 -0600

    testform3

--- C# ASP/WebFormsFrameworkPractice/WebApp3/Site.Master.designer.cs
+++ C# ASP/WebFormsFrameworkPractice/WebApp3/Site.Master.designer.cs
@@ -7,38 +7,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WebFormsDemo
+
+namespace WebApp3
 {
-
-
-    public partial class index
+    public partial class SiteMaster
     {
 
         /// <summary>
-        /// form1 control.
+        /// HeadContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
 
         /// <summary>
-        /// Button1 control.
+        /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-
-        /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
