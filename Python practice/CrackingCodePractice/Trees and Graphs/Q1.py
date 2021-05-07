f867a0304f3a98148bb534d8b623a4ddb999f663
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-07 12:24:03 -0600

    Update Q1.py

--- Python practice/CrackingCodePractice/Trees and Graphs/Q1.py
+++ Python practice/CrackingCodePractice/Trees and Graphs/Q1.py
@@ -1,2 +1,28 @@
 #graph traversal
 
+class Node:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+class Queue: 
+    def __init__(self):
+        self.size = 0
+        self.front = None
+        self.tail = None
+    def push(val):
+        if self.size == 0:
+            self.front = Node(val, None)
+            self.tail = self.front
+        else:
+            temp = Node(val, None)
+            self.tail.next = temp
+            self.tail = self.tail.next
+    def pop():
+        returnVal = self.head
+        self.head = self.head.next
+        return returnVal
+
+def traverse():
+
+        
