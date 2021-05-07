559994b0a9bfbea86073cb2204380c401550f558
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-07 13:40:34 -0600

    Update Q5.py

--- Python practice/CrackingCodePractice/Trees and Graphs/Q5.py
+++ Python practice/CrackingCodePractice/Trees and Graphs/Q5.py
@@ -58,25 +58,6 @@ class LL:
             return False
         return True
 
-    
-    
-    # def validate(self):
-    #     ptr = self.root
-    #     val = None
-    #     return self._val(ptr, val)
-        
-
-    # def _val(self, ptr, val):
-    #     if not ptr:
-    #         return True
-    #     if not self._val(ptr.left, val):
-    #         return False
-    #     if val and val > ptr.val:
-    #         return False
-    #     val = ptr.val
-    #     if not self._val(ptr.right, val):
-    #         return False
-    #     return True
 
 
 x = [i*i for i in range(10)]
