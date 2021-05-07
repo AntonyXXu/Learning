f6a42f769f6c6472dc58161a1107176074b1654a
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-07 15:40:19 -0600

    Update Q7.py

--- Python practice/CrackingCodePractice/Trees and Graphs/Q7.py
+++ Python practice/CrackingCodePractice/Trees and Graphs/Q7.py
@@ -1,1 +1,8 @@
 #graph, topological sort
+
+def topologicalSort(projects, dependencies):
+    graph = buildGraph(projects, dependencies)
+    return sortGraph(projects, dependencies)
+
+def buildGraph(projects, dependencies):
+    
