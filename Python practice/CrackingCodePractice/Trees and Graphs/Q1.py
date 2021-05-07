e99b2ac21c5da3274bdfba725a7247a92d75cd0b
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-07 12:26:45 -0600

    queue for breadth first traversal

--- Python practice/CrackingCodePractice/Trees and Graphs/Q1.py
+++ Python practice/CrackingCodePractice/Trees and Graphs/Q1.py
@@ -10,19 +10,26 @@ class Queue:
         self.size = 0
         self.front = None
         self.tail = None
-    def push(val):
+    def push(self, val):
         if self.size == 0:
             self.front = Node(val, None)
             self.tail = self.front
+            
         else:
             temp = Node(val, None)
             self.tail.next = temp
             self.tail = self.tail.next
-    def pop():
-        returnVal = self.head
-        self.head = self.head.next
+        self.size += 1
+    def pop(self):
+        returnVal = self.front
+        self.front = self.front.next
+        self.size-=1
         return returnVal
 
-def traverse():
+q = Queue()
+q.push(1)
+q.push(2)
+while q.size!= 0:
+    print(q.pop().val)
 
         
