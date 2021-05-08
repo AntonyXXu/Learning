1b72b32a5e016a1f0d20105eca83dbdbb6dcc75a
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-08 14:46:27 -0600

    Update Q3.py

--- Python practice/CrackingCodePractice/Search and Sort/Q3.py
+++ Python practice/CrackingCodePractice/Search and Sort/Q3.py
@@ -1,1 +1,42 @@
 #Search in rotated sorted array
+x = [5,6,7,9,-4,3,4]
+
+def findPivot(arr, low, high):
+    if low >= high:
+        return low
+    index = (low+high)//2
+    if index < 1 or index >= len(arr)-1:
+        return 0
+    if arr[index] > arr[index+1]:
+        return index+1
+    if arr[index-1] > arr[index]:
+        return index
+    if arr[index] > arr[0]:
+        return findPivot(arr, index+1, high)
+    else:
+        return findPivot(arr, low, index-1)
+
+def findVal(arr, val):
+    if len(arr) < 1:
+        return -1
+    piv = findPivot(x, 0, len(arr)-1)
+    i = 0
+    j = len(arr)-1
+    if val == arr[piv]:
+        return piv
+    if val >= arr[piv] and val <= arr[len(arr)-1]:
+        i = piv+1
+    else:
+        j = piv-1
+    while i <= j:
+        mid = (i + j) // 2
+        if arr[mid] == val:
+            return mid
+        if val < arr[mid]:
+            j = mid - 1
+        else: 
+            i = mid + 1       
+    return -1
+
+print(findVal(x, 3))
+print(findVal([1],1))
