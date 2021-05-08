37fbd84913760ac2476f05b87ad475d283bd57d3
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-08 15:30:50 -0600

    edge cases

--- Python practice/CrackingCodePractice/Search and Sort/Q5.py
+++ Python practice/CrackingCodePractice/Search and Sort/Q5.py
@@ -1,2 +1,38 @@
 #Sparse String
 
+x = ["", "at", "", "", "ball", "", "", "cop", "", "", "", "", "dad", "", ""]
+
+def find(arr, string):
+    i = 0
+    j = len(arr)-1
+    while i <= j:
+        mid = (i + j) // 2
+        
+        if arr[mid] == "":
+            left = mid - 1
+            right = mid + 1
+            while True:
+                if left >= i and arr[left] == "":
+                    left -= 1
+                else:
+                    mid = left
+                    break
+                if right <= j and arr[right] == "":
+                    right += 1
+                else:
+                    mid = right
+                    break
+                if left < i and right > j:
+                    return -1
+        if mid < i or mid > j:
+            return -1
+        if arr[mid] == string:
+            return mid
+        if string < arr[mid]:
+            j = mid - 1
+        else:
+            i = mid + 1
+    return -1
+
+print(find (x, "cop"))
+                
