ed3ee8ff64b20a99f6c9e9e1437f61f5b2ad97de
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-08 19:45:54 -0600

    Create Q11.py

---
+++ Python practice/CrackingCodePractice/Search and Sort/Q11.py
@@ -0,0 +1,13 @@
+#Peaks and Valleys
+
+arr = [5,3,1,2,3]
+arr1 = [9,1,0,4,8,7]
+
+def pv(arr):
+    res = sorted(arr)
+    for i in range(1,len(res),2):
+        res[i], res[i-1] = res[i-1], res[i]
+    return res
+
+print(pv(arr))
+print(pv(arr1))
