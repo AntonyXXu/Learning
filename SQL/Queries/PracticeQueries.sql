655936f82912f8792d125bdfc0b088f07e8d9a64
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-09 15:19:46 -0600

    Update PracticeQueries.sql

--- SQL/Queries/PracticeQueries.sql
+++ SQL/Queries/PracticeQueries.sql
@@ -1,5 +1,12 @@
+USE AP
+
 SELECT [VendorID]
       ,[LastName]
       ,[FirstName]
 	  , LEFT(FirstName,1) + LEFT(LastName,1) AS Initials
 FROM AP.dbo.ContactUpdates
+
+SELECT InvoiceDate, 
+	GETDATE() AS Today, 
+	DATEDIFF(YEAR, InvoiceDate, GETDATE()) AS InvoiceAge
+FROM Invoices
