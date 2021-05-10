9ed35738ceeebba4de2c5bfbd04dd472d91f295d
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-09 21:13:23 -0600

    Update JoinQueries.sql

--- SQL/Queries/JoinQueries.sql
+++ SQL/Queries/JoinQueries.sql
@@ -1,3 +1,4 @@
+USE AP
 
 SELECT InvoiceNumber, VendorName
 FROM Vendors JOIN Invoices
@@ -11,4 +12,22 @@ ORDER BY InvoiceDate
 
 -- Join More Tables
 SELECT VendorName, InvoiceNumber, InvoiceDate, InvoiceLineItemAmount as Amount, AccountDescription
-FROM 
+FROM Invoices
+	JOIN Vendors ON Invoices.VendorID = Vendors.VendorID
+	JOIN InvoiceLineItems ON Invoices.InvoiceID = InvoiceLineItems.InvoiceID
+	JOIN GLAccounts ON InvoiceLineItems.AccountNo = GLAccounts.AccountNo
+ORDER BY VendorName, InvoiceLineItemAmount DESC
+
+--Left outer join
+Select VendorName, InvoiceNumber, InvoiceTotal 
+FROM Vendors
+	LEFT JOIN Invoices ON Vendors.VendorID = Invoices.VendorID
+
+--Cross Join
+Select *
+FROM Vendors CROSS JOIN Invoices
+
+SELECT VendorName, InvoiceDate, InvoiceID
+FROM Vendors
+	JOIN Invoices ON invoices.VendorID = Vendors.VendorID
+	WHERE InvoiceDate > '2016-01-01'
