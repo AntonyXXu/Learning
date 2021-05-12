2811d9ce271da352dd605b515bf8bad6f970d451
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-12 14:48:44 -0600

    Customer Controller

--- C# ASP/SportsPro/SportsPro/Controllers/CustomerController.cs
+++ C# ASP/SportsPro/SportsPro/Controllers/CustomerController.cs
@@ -9,9 +9,76 @@ namespace SportsPro.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private SportsProContext context { get; set; }
+        public CustomerController(SportsProContext ctx)
         {
-            return View();
+            context = ctx;
+
+        }
+
+        public IActionResult List()
+        {
+            List<Product> products = context.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new Product());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Product prod = context.Products.Find(id);
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product prod)
+        {
+            try
+            {
+                context.Products.Add(prod);
+                context.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product prod)
+        {
+            try
+            {
+                context.Products.Update(prod);
+                context.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Product prod = context.Products.Find(id);
+                context.Products.Remove(prod);
+                context.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            catch
+            {
+                return View("List");
+            }
         }
     }
 }
