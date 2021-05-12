30520874d2ddc6dbed210d1f658b25d0a91d5394
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-11 22:10:28 -0600

    products page

--- C# ASP/SportsPro/SportsPro/Controllers/ProductController.cs
+++ C# ASP/SportsPro/SportsPro/Controllers/ProductController.cs
@@ -21,5 +21,64 @@ namespace SportsPro.Controllers
             List<Product> products = context.Products.ToList();
             return View(products);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new Product());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Product prod = context.Products.Find(id);
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product prod)
+        {
+            try
+            {
+                context.Products.Add(prod);
+                context.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product prod)
+        {
+            try
+            {
+                context.Products.Update(prod);
+                context.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Product prod = context.Products.Find(id);
+                context.Products.Remove(prod);
+                context.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            catch
+            {
+                return View("List");
+            }
+        }
     }
 }
