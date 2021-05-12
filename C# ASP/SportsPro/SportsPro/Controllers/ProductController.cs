9a003aee5d5b331247e44bc914dc45756fe2a36c
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-11 19:37:43 -0600

    products

---
+++ C# ASP/SportsPro/SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsPro.Models;
+
+namespace SportsPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        //private SportsProContext context { get; set; }
+        //public ProductController(SportsProContext ctx)
+        //{
+        //    context = ctx;
+        //}
+
+        public IActionResult List()
+        {
+            //List<Product> products = context.Products.ToList();
+            return View();
+        }
+    }
+}
