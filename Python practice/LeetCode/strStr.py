3a70a5b75d800598306c70c022af5c6d0e33e50d
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-15 19:32:38 -0600

    strStr()

---
+++ Python practice/LeetCode/strStr.py
@@ -0,0 +1,22 @@
+#Implement strStr()
+
+# return first instance index
+
+def strStr(s1, s2):
+    if s1 == s2:
+        return 0
+    for i in range(len(s1)):
+        eq = True
+        for j in range(len(s2)):
+            if s1[i+j] != s2[j]:
+                eq = False
+                break
+        if eq:
+            return i
+    return -1
+
+x = "hello"
+y = "o"
+
+print(strStr("",""))
+print(strStr(x,y))
