bdc424020babb2c0290cb99062cb7c173722b4f1
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-15 22:03:37 -0600

    Create Deepest_leaf_sum.py

---
+++ Python practice/LeetCode/Deepest_leaf_sum.py
@@ -0,0 +1,27 @@
+# Given the root of a binary tree, return the sum of values of its deepest leaves.
+
+class Node:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right= right
+
+deepest = 0
+sum = 0
+def deepestSum(root):
+    sum = 0
+    deepest = 0
+    helper(root, 0)
+    return sum
+    
+
+def helper(node, level):
+    if not node:
+        return 0
+    if level > deepest:
+        deepest = level
+    elif level == deepest:
+        sum += node.val
+    helper(node.left, level+1)
+    helper(node.right, level+1)
+
