74f4ac7ca19dffc0fd83283c7ce3d2ddadf9c23f
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-15 22:25:30 -0600

    Update Deepest_leaf_sum.py

--- Python practice/LeetCode/Deepest_leaf_sum.py
+++ Python practice/LeetCode/Deepest_leaf_sum.py
@@ -26,3 +26,22 @@ def helper(node, level):
     helper(node.left, level+1)
     helper(node.right, level+1)
 
+def deepestSumQueue(root):
+    if not root:
+        return 0
+    total = 0
+    q = [root]
+    while len(q):
+        total = 0
+        for i in range(len(q)):
+            curr = q[0]
+            q.pop(0)
+            if curr.left:
+                q.append(curr.left)
+            if curr.right:
+                q.append(curr.right)
+            else:
+                total += curr.val
+    return total
+    
+            
