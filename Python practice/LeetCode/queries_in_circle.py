bc5425fa182fdcc78e85a3b3af371501c6703ed8
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-16 22:07:33 -0600

    Update queries_in_circle.py

--- Python practice/LeetCode/queries_in_circle.py
+++ Python practice/LeetCode/queries_in_circle.py
@@ -13,3 +13,21 @@ queries = [[2,3,1],[4,3,1],[1,1,2]]
 points1 = [[1,1],[2,2],[3,3],[4,4],[5,5]]
 queries1 = [[1,2,2],[2,2,2],[4,3,2],[4,3,3]]
 
+def query_in_circles(points, queries):
+    res = [0] * len(queries)
+    for i in range(len(queries)):
+        for point in points:
+            if inside(point, queries[i]):
+                res[i] += 1
+    return res
+    
+
+def inside(point, circle):
+    radius = circle[2]
+    return (circle[0] - radius <= point[0]
+    and point[0] <= circle[0] + radius
+    and circle[1] - radius <= point[1]
+    and point[1] <= circle[1] + radius)
+
+print(query_in_circles(points,queries))
+print(query_in_circles(points1,queries1))
