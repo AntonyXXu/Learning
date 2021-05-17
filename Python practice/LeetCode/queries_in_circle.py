b99feef066e07fac914d78ef428f962fd07512b5
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-16 22:17:07 -0600

    Update queries_in_circle.py

--- Python practice/LeetCode/queries_in_circle.py
+++ Python practice/LeetCode/queries_in_circle.py
@@ -31,3 +31,41 @@ def inside(point, circle):
 
 print(query_in_circles(points,queries))
 print(query_in_circles(points1,queries1))
+
+def lowSearch(points, lowX):
+    left = 0
+    right = len(points)
+    while left < right:
+        mid = (left + right) // 2
+        if lowX <= points[mid][0]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+    
+def highSearch(points, highX):
+    left = 0
+    right = len(points)
+    while left < right:
+        mid = (left + right) // 2
+        if highX >= points[mid][0]:
+            left = mid + 1
+        else:
+            right = mid
+    return right
+
+def optimized(points,queries):
+    points.sort()
+    res = [0] * len(queries)
+    for i in range(len(queries)):
+        left = lowSearch(points, queries[i][0] - queries[i][2])
+        right = lowSearch(points, queries[i][0] + queries[i][2])
+        for point in points[left:right+1]:
+            if (queries[i][1] - queries[i][2] <= point[1]
+            and point[1] <= queries[i][1] + queries[i][2]):
+                res[i] += 1
+    return res
+    
+print(optimized(points,queries))
+print(optimized(points1,queries1))
+x = [1,2,3]
