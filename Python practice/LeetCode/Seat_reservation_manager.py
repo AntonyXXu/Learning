c79661b081ea6bcafb591857aecf77ef71acf867
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-18 17:28:37 -0600

    heapified seat manager

--- Python practice/LeetCode/Seat_reservation_manager.py
+++ Python practice/LeetCode/Seat_reservation_manager.py
@@ -22,4 +22,32 @@ class SeatManager:
         self.seats[seat] = False
         return True
 
+class SeatManagerHeap:
+    def __init__(self, num):
+        self.seats = [i for i in range(1, num+1)]
+        self.count = num
+
+    def reserve(self):
+        if not self.seats or self.count == 0:
+            return -1
+        ret_val = self.seats[0]
+        self.count -= 1
+        i = 0
+        while (2*i+1 < self.count or 2*i+2 < self.count 
+        or self.seats[i] < self.seats[2*i+1] or self.seats[i] < self.seats[2*i+2]):
+            if self.seats[2*i+1] > self.seats[2*i+2]:
+                self.seats[i], self.seats[2*i+1] = self.seats[2*i+1], self.seats[i]
+                i = 2*i + 1
+            else:
+                self.seats[i], self.seats[2*i + 2] = self.seats[2*i + 2], self.seats[i]
+                i = 2*i + 2
+        return ret_val
+
+    def unreserve(self, num):
+        self.seats[self.count] = num
+        i = self.count
+        self.count += 1
+        while (i-1)//2 < 0 or self.seats[i] < self.seats[(i-1)//2]:
+            self.seats[i], self.seats[(i-1)//2] = self.seats[(i-1)//2], self.seats[i]
+        return
 
