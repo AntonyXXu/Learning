3274438d3a366c015c46c25de439177054803e81
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-18 21:40:23 -0600

    Update Heap.py

--- Simple Algorithms and Data Structures/Heap.py
+++ Simple Algorithms and Data Structures/Heap.py
@@ -17,7 +17,38 @@ class minHeap:
         i = self.count
         self.heap[i] = val
         self.count += 1
-        while val < self.heap[(i-1)//2]:
+        while i > 0 and self.heap[i] < self.heap[(i-1)//2]:
             self.heap[i], self.heap[(i-1)//2] = self.heap[(i-1)//2], self.heap[i]
             i = (i-1)//2
 
+    def pop(self):
+        if self.count == 0:
+            return
+        retVal = self.heap[0]
+        i = 0
+        self.heap[i] = self.heap[self.count-1]
+        while (2*i+1 < self.count and 2*i+2 < self.count and
+        (self.heap[i] > self.heap[2*i+1] or self.heap[i] > self.heap[2*i+2])):
+            if self.heap[2*i+1] < self.heap[2*i+2] :
+                self.heap[i], self.heap[2*i+1] = self.heap[2*i+1], self.heap[i] 
+                i = 2*i + 1
+            else:
+                self.heap[i], self.heap[2*i+2]= self.heap[2*i+2], self.heap[i] 
+                i = 2*i + 2
+        self.heap[self.count-1] = 0
+        self.count -= 1
+        return retVal
+    
+test = minHeap(5)
+test.insert(4)
+test.insert(3)
+test.insert(5)
+test.insert(10)
+test.insert(1)
+test.insert(15)
+print(test.heap)
+print(test.pop())
+print(test.pop())
+print(test.pop())
+print(test.heap)
+print(test.count)
