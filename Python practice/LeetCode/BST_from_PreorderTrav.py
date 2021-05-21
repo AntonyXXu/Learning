4b7169ed0ba539dfa572124cce6218d0a141e80a
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-20 20:28:27 -0600

    Update BST_from_PreorderTrav.py

--- Python practice/LeetCode/BST_from_PreorderTrav.py
+++ Python practice/LeetCode/BST_from_PreorderTrav.py
@@ -10,20 +10,38 @@ class Node:
         self.right = right
         self.left = left
 
+# def build_preorder(arr):
+#     if not arr or len(arr) == 0:
+#         return None
+#     root, i = helper(arr, 0, arr[len(arr)-1])
+#     return root
+
+# def helper(arr, i, maxval):
+#     if i >= len(arr) or arr[i] > maxval:
+#         return None, i
+#     node = Node(arr[i])
+#     i += 1
+#     node.left, i = helper(arr, i, node.val)
+#     node.right, i = helper(arr, i, maxval)
+#     return node, i
+
 def build_preorder(arr):
+    asd = 1
     if not arr or len(arr) == 0:
         return None
-    root, i = helper(arr, 0, arr[len(arr)-1])
+    root, index = helper(arr, 0, arr[len(arr)-1])
     return root
+    
+def helper(arr, index, maxv):
+    if index >= len(arr) or arr[index] > maxv:
+        return None, index
+    node = Node(arr[index])
+    index += 1
+    node.left, index = helper(arr, index , node.val)
+    node.right, index = helper(arr, index , maxv)
+    return node, index
 
-def helper(arr, i, maxval):
-    if i >= len(arr) or arr[i] > maxval:
-        return None, i
-    node = Node(arr[i])
-    i += 1
-    node.left, i = helper(arr, i, node.val)
-    node.right, i = helper(arr, i, maxval)
-    return node, i
+        
 
 
 
