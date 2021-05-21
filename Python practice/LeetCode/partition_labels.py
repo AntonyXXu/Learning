52aabc85189b92b2f3b7df9d97e9147af7f86bec
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-20 23:18:14 -0600

    Create partition_labels.py

---
+++ Python practice/LeetCode/partition_labels.py
@@ -0,0 +1,31 @@
+# A string s of lowercase English letters is given. We want to partition this string into as many parts as possible 
+# so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+
+# https://leetcode.com/problems/partition-labels/
+
+s = "ababcbacadefegdehijhklij"
+
+def partition_labels(string):
+    res = []
+    if not string or len(string) == 0:
+        return res
+    chars = {}
+    for index, letter in enumerate(string):
+        if letter not in chars:
+            chars[letter] = [index, len(string)]
+        else:
+            chars[letter][1] = index
+    min_i = None
+    max_i = None
+    for start, end in chars.values():
+        if min_i == None and max_i == None:
+            min_i = start
+            max_i = end
+        elif start < max_i and end > max_i:
+            max_i = end
+        elif start > max_i:
+            res.append(max_i - min_i + 1)
+            max_i, min_i = None, None
+    return res
+
+print(partition_labels(s))
