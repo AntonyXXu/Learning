3f1e55bd9531052b5465a40861bc35d760ca0feb
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-25 19:33:57 -0600

    feathers update

--- Javascript/Feathers.js/codingGardenTutorial/app.js
+++ Javascript/Feathers.js/codingGardenTutorial/app.js
@@ -1,45 +1,44 @@
-const feathers = require('@feathersjs/feathers');
-const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
+const feathers = require("@feathersjs/feathers");
+const express = require("@feathersjs/express");
+const socketio = require("@feathersjs/socketio");
 const app = express(feathers());
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.configure(express.rest());
-app.configure(socketio()); 
-
+app.configure(socketio());
 
 class MessageService {
-    constructor() {
-        this.messages = [];
-    }
-    async find() {
-        return this.messages;
-    }
-    async create(data) {
-        const message = {
-            id : this.messages.length,
-            text : data.text
-        }
-        this.messages.push(message);
-    }
+  constructor() {
+    this.messages = [];
+  }
+  async find() {
+    return this.messages;
+  }
+  async create(data) {
+    const message = {
+      id: this.messages.length,
+      text: data.text,
+    };
+    this.messages.push(message);
+  }
 }
 
-app.use('/messages', new MessageService());
+app.use("/messages", new MessageService());
 app.use(express.errorHandler());
 
-app.on('connection', connection => {
-    app.channel('everybody'.join(connection));
-})
+app.on("connection", (connection) => {
+  app.channel("everybody".join(connection));
+});
 
-app.publish( () => app.channel('everybody'));
+app.publish(() => app.channel("everybody"));
 
-app.listen(3030).on('listening', ()=> {
-    console.log("feathers server on localhost:"+3030)
-})
+app.listen(3030).on("listening", () => {
+  console.log("feathers server on localhost:" + 3030);
+});
 
-app.service('messages').create( {
-    text : 'hello'
+app.service("messages").create({
+  text: "hello",
 });
