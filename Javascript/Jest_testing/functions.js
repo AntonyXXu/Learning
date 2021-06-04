e78f95de0761a0786449062d0012489a0bd88945
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-03 18:20:28 -0600

    tobe jest test

--- Javascript/Jest_testing/functions.js
+++ Javascript/Jest_testing/functions.js
@@ -2,6 +2,14 @@ const functions = {
   add: (a, b) => {
     return a + b;
   },
+  isNull: (x) => {
+    return x;
+  },
+  createUser = () => {
+      const user = {firstName: "abc"}
+      user["lastName"] = "cde"
+      return user
+  }
 };
 
 module.exports = functions;
--- Javascript/Jest_testing/functions.test.js
+++ Javascript/Jest_testing/functions.test.js
@@ -3,3 +3,11 @@ const functions = require("./functions");
 test("Add nums", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Add nums", () => {
+  expect(functions.add(2, 2)).not.toBe(6);
+});
+
+test("return null", () => {
+  expect(functions.isNull(null)).toBeFalsy();
+});
