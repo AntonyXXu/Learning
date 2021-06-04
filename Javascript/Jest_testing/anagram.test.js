45611bf86d2228612a2e3a29ad72ef306721af07
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-03 19:19:28 -0600

    watchAll jest

--- Javascript/Jest_testing/anagram.test.js
+++ Javascript/Jest_testing/anagram.test.js
@@ -1,5 +1,44 @@
 const isAnagram = require("./anagram");
 
+// beforeEach(() => {
+//   return initDB();
+// });
+// afterEach(() => {
+//   return closeDB();
+// });
+
+// beforeAll(() => {
+//   return initDB();
+// });
+// afterAll(() => {
+//   return closeDB();
+// });
+
+// const initDB = () => {
+//   console.log("init db");
+// };
+// const closeDB = () => {
+//   console.log("close DB");
+// };
+const nameCheck = () => {
+  console.log("checking name");
+};
+
+describe("Checking Names", () => {
+  beforeEach(() => {
+    return nameCheck();
+  });
+  test("user is jeff", () => {
+    const user = "jeff";
+    expect(user).toEqual("jeff");
+  });
+
+  test("user is karen", () => {
+    const user = "karen";
+    expect(user).toEqual("karen");
+  });
+});
+
 test("fcn exists", () => {
   expect(typeof isAnagram).toBe("function");
 });
