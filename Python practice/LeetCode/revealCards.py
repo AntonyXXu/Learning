6fcf870df88dae1e9f71f60867b0fc14b0dbabe6
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-05 17:40:40 -0600

    revealCards

--- Python practice/LeetCode/revealCards.py
+++ Python practice/LeetCode/revealCards.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/reveal-cards-in-increasing-order/
+import collections
+
 c = [17,13,11,2,3,5,7]
 
 def revealIncreasing(cards):
@@ -11,3 +13,18 @@ def revealIncreasing(cards):
     return res
 
 print(revealIncreasing(c))
+
+def dequeIncreasing(cards):
+    sortedCards = sorted(cards, reverse=True)
+    deq = collections.deque()
+    deq.appendleft(sortedCards[0])
+    for i in range(1,len(sortedCards)):
+        deq.appendleft(deq.pop())
+        deq.appendleft(sortedCards[i])
+    res = []
+    for i in range(len(sortedCards)):
+        res.append(deq.popleft())
+
+    return res
+
+print(dequeIncreasing(c))
