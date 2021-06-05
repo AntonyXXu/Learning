f08543361a77f6c5956d78ce19d03f3ace796b41
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-04 22:44:13 -0600

    provider facebook get

--- Javascript/Jest_testing/api.test.jest.ts
+++ Javascript/Jest_testing/api.test.jest.ts
@@ -52,4 +52,57 @@ describe("lambda fcn client-config-svc", () => {
     expect(response.body).toHaveProperty("status");
     expect(response.body).toHaveProperty("country");
   });
+
+  it("GET /api/providers/ can respond", async () => {
+    //Arrange
+    const data = new TextEncoder().encode(
+      JSON.stringify({
+        path: `/client-config-svc/api/providers/`,
+        httpMethod: "GET",
+      })
+    );
+    const expected = ["semrush", "wine-expo"];
+    const notExpected = ["testProvider1", "testProvider2"];
+    //Act
+    const response = await LambdaClient.invoke({
+      FunctionName: "client-config-svc",
+      payload: data,
+    });
+    //Assert
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body).toBe(Array);
+    expect(response.body).toEqual(expect.arrayContaining(expected));
+    expect(response.body).not.toEqual(expect.arrayContaining(notExpected));
+  });
+
+  it("GET /api/providers/facebook can respond", async () => {
+    //Arrange
+    const provider = "facebook";
+    const data = new TextEncoder().encode(
+      JSON.stringify({
+        path: `/client-config-svc/api/providers/${provider}`,
+        httpMethod: "GET",
+      })
+    );
+
+    //Act
+    const response = await LambdaClient.invoke({
+      FunctionName: "client-config-svc",
+      payload: data,
+    });
+    //Assert
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body).toBe(Array);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("providerID");
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("clientID");
+    expect(response.body).toHaveProperty("type");
+    expect(response.body).toHaveProperty("granularity");
+    expect(response.body).toHaveProperty("status");
+    expect(response.body).toHaveProperty("isCustom");
+    expect(response.body).toHaveProperty("domain");
+    expect(response.body).toHaveProperty("resources");
+    expect(response.body).toHaveProperty("historicalPullInMonts");
+  });
 });
