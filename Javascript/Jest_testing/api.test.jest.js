7c809de10fecabf243cc6bac68ffaae11e468219
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-04 22:03:28 -0600

    added additional tests

--- Javascript/Jest_testing/api.test.jest.js
+++ Javascript/Jest_testing/api.test.jest.js
@@ -4,11 +4,35 @@ import { MyLambdaClient } from "aws-sdk";
 const LambdaClient = new MyLambdaClient();
 
 describe("lambda fcn client-config-svc", () => {
-  it("GET can respond", async () => {
+  it("GET /api/clients can respond", async () => {
     //Arrange
     const data = new TextEncoder("UTF-8").encode(
       JSON.stringify({
-        path: "/client-config-svc/",
+        path: "/client-config-svc//api/clients",
+        httpMethod: "GET",
+      })
+    );
+    const expected = ["niagara-wineries"];
+    const notExpected = ["testName1", "testName2"];
+
+    //Act
+    const response = await LambdaClient.invoke({
+      FunctionName: "client-config-svc",
+      payload: data,
+    });
+
+    //Assert
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body).toBe(Array);
+    expect(response.body).toEqual(expect.arrayContaining(expected));
+    expect(response.body).not.toEqual(expect.arrayContaining(notExpected));
+  });
+
+  it("GET /api/clients can respond", async () => {
+    //Arrange
+    const data = new TextEncoder("UTF-8").encode(
+      JSON.stringify({
+        path: "/client-config-svc/api",
         httpMethod: "GET",
       })
     );
