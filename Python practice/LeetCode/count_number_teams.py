ba3a34237ab044ffdbd758b44170bf0d2c7fcdae
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-08 19:17:57 -0600

    countTeams optimized

--- Python practice/LeetCode/count_number_teams.py
+++ Python practice/LeetCode/count_number_teams.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/count-number-of-teams/
+from typing import List
+
+
 t1 = [2,5,3,4,1]
 t2 = [2,1,3]
 t3 = [1,2,3,4]
@@ -17,4 +20,28 @@ def numTeams(t):
 print(numTeams(t1))
 print(numTeams(t2))
 print(numTeams(t3))
-    
+
+def opt(t : List[int])-> int:
+    res = 0
+    for i in range(len(t)):
+        leftLess = 0
+        rightLess = 0
+        leftMore = 0
+        rightMore = 0
+        for j in range(len(t)):
+            if j < i:
+                if t[j] < t[i]:
+                    leftLess += 1
+                else:
+                    leftMore += 1
+            elif j > i:
+                if t[i] < t[j]:
+                    rightMore += 1
+                else:
+                    rightLess += 1
+        res += leftLess * rightMore + leftMore * rightLess
+    return res
+
+print(opt(t1))
+print(opt(t2))
+print(opt(t3))
