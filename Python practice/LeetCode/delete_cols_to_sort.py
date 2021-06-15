7f2bea09961de5781f0da5294e0d965a5560bd6c
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-14 20:08:50 -0600

    delete sort cols

---
+++ Python practice/LeetCode/delete_cols_to_sort.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/delete-columns-to-make-sorted/
+
+s1 =  ["cba","daf","ghi"]
+s2 = ["a","b"]
+s3 =  ["zyx","wvu","tsr"]
+
+def minDel(arr):
+    res = 0
+    
+    for col in range(len(arr[0])):
+        for row in range(1,len(arr)):
+            if ord(arr[row][col]) > ord(arr[row][0]):
+                res += 1
+                break
+    return res
+
+print(minDel(s1))
+print(minDel(s2))
+print(minDel(s3))
+        
+
