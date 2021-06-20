2fd4cac6c9b8a65d7335eb575d6c27d8799f5887
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-20 17:46:15 -0600

    plus one correct

--- Python practice/LeetCode/plus_one.py
+++ Python practice/LeetCode/plus_one.py
@@ -1,8 +1,18 @@
 def plusOne(lst):
-    lst[-1] += 1
+    carry = 0
+    for i in range(len(lst)-1, -1, -1):
+        lst[i] += 1
+        if lst[i] == 10:
+            lst[i] = 0
+            carry = 1
+        else:
+            return lst
+    if carry == 1:
+        lst.insert(0, 1)
     return lst
 
-print(plusOne([1,2,3]))
+print(plusOne([1,2,9]))
 print(plusOne([1,2,0]))
 print(plusOne([4,3,2,1]))
 print(plusOne([0]))
+print(plusOne([9,9,9]))
