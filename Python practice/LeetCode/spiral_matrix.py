a3558afb0e72254e85cec76b13b3a29deb2d67a4
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-19 18:11:44 -0600

    spiralMatrix correct

--- Python practice/LeetCode/spiral_matrix.py
+++ Python practice/LeetCode/spiral_matrix.py
@@ -14,27 +14,28 @@ def spiral(rows, cols, rStart, cStart):
     r = rStart
     c = cStart
     inc = 1
+    res.append([r,c])
     while len(res) < rows * cols:
-        res.append([r,c])
-        for i in range(inc):
-            r += inc
-            if r < rows and c < cols and r >= 0 and c >= 0:
-                res.append([r,c])
         for j in range(inc):
-            c += inc
+            c += 1
             if r < rows and c < cols and r >= 0 and c >= 0:
                 res.append([r,c])
+        for i in range(inc):
+            r += 1
+            if r < rows and c < cols and r >= 0 and c >= 0:
+                res.append([r,c])
+
         inc += 1
         for i in range(inc):
-            r -= inc
+            c -= 1
             if r < rows and c < cols and r >= 0 and c >= 0:
                 res.append([r,c])
         for j in range(inc):
-            r -= inc
+            r -= 1
             if r < rows and c < cols and r >= 0 and c >= 0:
                 res.append([r,c])
         inc += 1
     return res
 
 print(spiral(r,c,rs,cs))
-print(spiral(rows,cols,rStart,cStart))
+print(spiral(rows,cols,rStart,cStart) == [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]])
