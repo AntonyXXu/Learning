e8ca9b8cb90b223e8f3c5eef41734f5bf36361bc
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-22 21:07:16 -0600

    looped linkedList

---
+++ Python practice/LeetCode/linked_list_loop.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+def loop(head):
+    if not head:
+        return False
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
