52289cba7a86df91a6ba6a46fea71c8754850393
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-27 17:56:17 -0600

    java interest calc

--- java/InterestCalculator/src/com/antonyxu/Main.java
+++ java/InterestCalculator/src/com/antonyxu/Main.java
@@ -1,8 +1,26 @@
 package com.antonyxu;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final int MONTHS = 12;
+        final int PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Principal: \t");
+        int principal = scanner.nextInt();
+
+        System.out.println("Interest: \t");
+        double annualInterest = scanner.nextDouble();
+        double monthlyInterest = annualInterest / MONTHS;
+
+        System.out.println("Years: \t");
+        int years = scanner.nextInt();
+
+
+        double mortgage = Math.pow(1+monthlyInterest / 100, years) * principal;
+        System.out.println("Mortgage: \t " + mortgage);
     }
 }
