3466ed142e264fb085e8d25877f2ebe1f8ba8879
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-27 16:38:34 -0600

    java string

--- java/MoshTutorial/src/com/antonyxu/Main.java
+++ java/MoshTutorial/src/com/antonyxu/Main.java
@@ -1,13 +1,32 @@
 package com.antonyxu;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	    int age = 30;
-	    long viewsCount = 40_000_000_000L;
-	    float price = 10.99F;
-	    char letter = 'A';
-	    boolean eligible = false;
-        System.out.println(age);
+//	    int age = 30;
+//	    long viewsCount = 40_000_000_000L;
+//	    float price = 10.99F;
+//	    char letter = 'A';
+//	    boolean eligible = false;
+//      System.out.println(age);
+//
+//        int age = 20;
+//        Date now = new Date();
+//        System.out.println(now);
+//
+//        Point pt1 = new Point(1,1);
+//        Point pt2 = pt1;
+//        pt1.x = 10;
+//        System.out.println(pt1.x);
+//        System.out.println(pt2);
+
+        String msg = "Hello \"me\"";
+        msg += '!';
+        System.out.println(msg);
+        String path = "c:\\Windows\\...";
+        System.out.println(path);
     }
 }
