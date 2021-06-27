77f3c193e504a1db79872bbec29378adccae9de5
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-06-27 16:30:14 -0600

    java types

---
+++ java/MoshTutorial/src/com/antonyxu/Main.java
@@ -0,0 +1,13 @@
+package com.antonyxu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int age = 30;
+	    long viewsCount = 40_000_000_000L;
+	    float price = 10.99F;
+	    char letter = 'A';
+	    boolean eligible = false;
+        System.out.println(age);
+    }
+}
