b0d6e5574683d6f3d322d36bd41408e2026a6c3a
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-03 20:20:24 -0600

    reconstruction by height alternate

--- Python practice/LeetCode/queue_reconstruction_by_height.py
+++ Python practice/LeetCode/queue_reconstruction_by_height.py
@@ -7,5 +7,19 @@ def reconstructQueue(people):
         ans.insert(sort_people[i][1], sort_people[i])
     return ans
 
+def reconstructQueue(people):
+    p = sorted(people, key=lambda x: [x[0], x[1]])
+    ans = [None] * len(people)
+    for person in p:
+        bigger = 0
+        position = 0
+        while position < len(p) and bigger <= person[1]:
+            if ans[position] == None or ans[position][0] >= person[0]:
+                bigger += 1
+            position += 1
+        ans[position-1] = person
+
+    return ans
+
 print(reconstructQueue([[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]))
 print(reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]))
