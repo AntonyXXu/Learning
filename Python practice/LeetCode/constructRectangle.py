cf4303645be7c0c6b78a166d1a127add89571db1
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-11 14:31:25 -0600

    constructRectangle

---
+++ Python practice/LeetCode/constructRectangle.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/construct-the-rectangle/
+
+def constructRectangle(area):
+    if area == 0:
+        return [0,0]
+    s = int(area**0.5) + 1
+    while s > 0:
+        if area % s == 0:
+            return [area // s, s]
+        s -= 1
+
+print(constructRectangle(37))
+print(constructRectangle(122122))
