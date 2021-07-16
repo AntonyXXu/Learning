82876d81e281e5b8929bf4ad36c494775061a259
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-15 19:18:03 -0600

    bfs lettercase

--- Python practice/LeetCode/letterCasingDP.py
+++ Python practice/LeetCode/letterCasingDP.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/letter-case-permutation/
+import collections
 
 def helper(prefix, index, s, ans):
     if len(prefix) == len(s):
@@ -14,6 +15,23 @@ def letterCase(s):
     ans = []
     a = helper('', 0, s, ans)
     return a
+
+# BFS
+def letterCase(s):
+    if not s or len(s) == 0:
+        return s
+    dq = collections.deque(s)
+    for i in range(len(s)):
+        c = s[i]
+        if c.isalpha():
+            for i in range(len(s)):
+                string = dq.popleft()
+                left = string[:i]
+                right = string[i+1:]
+
+                dq.append(left + c + right)
+                dq.append(left + c.swapcase() + right)
+    return ''.join(list(dq))
 
 print(letterCase(s = "a1b2"))
 print(letterCase(s = "3z4"))
