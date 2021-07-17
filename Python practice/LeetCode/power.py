4afa501b5138ec604742ee32c1be2650d9398837
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-16 18:59:01 -0600

    power iterative

--- Python practice/LeetCode/power.py
+++ Python practice/LeetCode/power.py
@@ -9,3 +9,25 @@ def power(x, n):
         return power(x, n-1) * x
     else:
         return power (x * x, n // 2)
+
+def power(x, n):
+    if n == 0:
+        return 1
+    negative = False
+    if n < 0:
+        negative = True
+        n *= -1
+    ans = 1
+    while n > 0:
+        if n % 2 == 0:
+            x *= x
+            n //= 2
+        else:
+            ans *= x
+            n -= 1
+
+    if negative:
+        ans = 1 / ans
+    return ans
+
+
