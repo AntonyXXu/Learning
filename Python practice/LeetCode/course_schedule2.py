20d18dead900f3afa2b4869eb524979019f8456e
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-18 13:17:08 -0600

    topological sort bfs

--- Python practice/LeetCode/course_schedule2.py
+++ Python practice/LeetCode/course_schedule2.py
@@ -30,7 +30,31 @@ def dfs( course, visited, recursiveStack, ans, graph):
     recursiveStack[course] = False
     return True
 
+
+def findOrder(numCourses, prerequisites):
+    graph = collections.defaultdict(set)
+    in_degrees = collections.defaultdict(int)
+    for preReq in prerequisites:
+        graph[preReq[0]].add(preReq[1])
+        in_degrees[preReq[1]] += 1
+    dq = collections.deque()
+    for course in range(numCourses):
+        if not in_degrees[course]:
+            dq.append(course)
+
+    visited = set()
+
+    while dq:
+        course = dq.popleft()
+        visited.add(course)
+        for neighbor in graph[course]:
+            in_degrees[neighbor] -= 1
+            if not in_degrees[neighbor]:
+                dq.append(neighbor)
+    return len(visited) == numCourses
+
 print(findOrder(numCourses = 2, prerequisites = [[1,0]]))
 print(findOrder(numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]))
 print(findOrder(numCourses = 1, prerequisites = []))
+print(findOrder( numCourses = 2, prerequisites = [[1,0],[0,1]]))
 print(findOrder(2,[[0,1]]))
