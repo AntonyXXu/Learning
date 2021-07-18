95e94b57d25d3dae4fe31a397544a2ebe9675217
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-17 18:01:51 -0600

    correct setMatrix Zeroes

--- Python practice/LeetCode/set_matrix_zeroes.py
+++ Python practice/LeetCode/set_matrix_zeroes.py
@@ -3,11 +3,20 @@
 def setZeroes(matrix):
     if not matrix:
         return matrix
-    first = False
-    if matrix[0][0] == 0:
-        first = True
+    firstRow = False
+    firstCol = False
     rows = len(matrix)
     cols = len(matrix[0])
+
+    for row in range(rows):
+        if matrix[row][0] == 0:
+            firstRow = True
+            break
+
+    for col in range(cols):
+        if matrix[0][col] == 0:
+            firstCol = True
+            break
 
     for row in range(rows):
         for col in range(cols):
@@ -26,9 +35,10 @@ def setZeroes(matrix):
             for row in range(rows):
                 matrix[row][col] = 0
 
-    if first:
+    if firstRow:
         for row in range(rows):
             matrix[row][0] = 0
+    if firstCol:
         for col in range(cols):
             matrix[0][col] = 0
     
@@ -37,3 +47,4 @@ def setZeroes(matrix):
 
 print(setZeroes(    [[1,1,1],[1,0,1],[1,1,1]]  ) ==[[1,0,1],[0,0,0],[1,0,1]] )
 print(setZeroes(   [[0,1,2,0],[3,4,5,2],[1,3,1,5]]    ) ==[[0,0,0,0],[0,4,5,0],[0,3,1,0]])
+print(setZeroes([[1,0,3]]))
