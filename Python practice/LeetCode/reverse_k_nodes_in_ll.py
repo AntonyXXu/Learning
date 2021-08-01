a90f0f495949fb28e29d8be14c33b74dc8dde249
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-08-01 13:57:05 -0600

    reverse k nodes in linkedlist

---
+++ Python practice/LeetCode/reverse_k_nodes_in_ll.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/reverse-nodes-in-k-group/
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseKGroup(head, k):
+    if k == 1:
+        return head
+    dummy = ListNode(0, head)
+    pre = dummy
+    left = right = head
+    while right:
+        ctr = 0
+        while ctr < k and right:
+            ctr += 1
+            right = right.next
+        if ctr == k:
+            prev = right
+            curr = left
+            for _ in range(k):
+                temp = curr
+                curr.next = prev
+                curr = curr.next
+                prev = temp
+            pre.next = right
+            pre = left
+            left = right
+        if not right:
+            return dummy.next
+    return dummy.next
