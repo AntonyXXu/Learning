3e7aab186d22243a908333c77057f7d1a052f522
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-08-09 19:44:54 -0600

    update circular graph

--- Python practice/LeetCode/detect_circular_graph.py
+++ Python practice/LeetCode/detect_circular_graph.py
@@ -2,6 +2,7 @@
 
 import collections
 
+
 class Graph():
     def __init__(self, vertices):
         self.graph = collections.defaultdict(list)
@@ -9,7 +10,7 @@ class Graph():
 
     def insert(self, edge):
         self.graph[edge[0]].append(edge[1])
-    
+
     def cyclic(self):
         visited = [False] * (self.v + 1)
         recursive = [False] * (self.v + 1)
@@ -28,16 +29,17 @@ class Graph():
                 if self.cyclicHelper(neighbor, visited, recursive):
                     return True
             elif recursive[neighbor]:
-                    return True
+                return True
         recursive[vertex] = False
         return False
-    
+
+
 g = Graph(4)
-g.insert([0,1])
-g.insert([0,2])
-g.insert([1,2])
-g.insert([2,0])
-g.insert([2,3])
-g.insert([3,3])
+g.insert([0, 1])
+g.insert([0, 2])
+g.insert([1, 2])
+g.insert([2, 0])
+g.insert([2, 3])
+g.insert([3, 3])
 
 print(g.cyclic())
