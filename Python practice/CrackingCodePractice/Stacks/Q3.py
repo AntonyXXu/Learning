f39deeb0d8547ec1dd17eae6ae89894de25631fb
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-08-17 17:56:34 -0600

    stacks

--- Python practice/CrackingCodePractice/Stacks/Q3.py
+++ Python practice/CrackingCodePractice/Stacks/Q3.py
@@ -1,4 +1,4 @@
-#stack of plates - array of stack, with popIndex()
+# stack of plates - array of stack, with popIndex()
 
 class SetOfStacks:
     def __init__(self, maxSize):
@@ -8,10 +8,10 @@ class SetOfStacks:
         self.currIndex = -1
 
     def push(self, val):
-        newAdd = [val]            
-        if not self.arrStack or len(self.arrStack[self.currIndex]) == self.maxSize :
+        newAdd = [val]
+        if not self.arrStack or len(self.arrStack[self.currIndex]) == self.maxSize:
             self.arrStack.append(newAdd)
-            self.currIndex +=1
+            self.currIndex += 1
         else:
             self.arrStack[self.currIndex].append(val)
         self.size += 1
@@ -23,21 +23,19 @@ class SetOfStacks:
         self.size -= 1
         if not self.arrStack[self.currIndex]:
             del self.arrStack[-1]
-            self.currIndex -=1
+            self.currIndex -= 1
         return popVal
 
     def popIndex(self, index):
         if not self.arrStack or index > self.currIndex:
             return None
         popVal = self.arrStack[index].pop()
-        self.size -= 1 
+        self.size -= 1
         if not self.arrStack[index]:
             del self.arrStack[index]
             self.currIndex -= 1
         return popVal
-    
-    def shift(self):
-        
+
 
 stk = SetOfStacks(3)
 
@@ -51,7 +49,6 @@ stk.push(1)
 stk.popIndex(1)
 stk.popIndex(1)
 stk.popIndex(1)
-
 
 
 print(stk.currIndex)
