983910705fff6dcc306e247079dd72c124afd1ef
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-08-19 19:29:03 -0600

    learn ruby

---
+++ ruby/basics/helloWorld.rb
@@ -0,0 +1,13 @@
+puts "new line"
+print "Hello world"
+print "same line"
+
+# vars
+testVar = "abc"
+
+# data types
+stringVar = "test"
+numberVar = 123
+decimalVar = 12.3
+boolVar = true
+nullType = nil
