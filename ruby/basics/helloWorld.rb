b5613c6e991f67195b2f8a0cf25f34e29c65c1d5
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-08-19 19:41:42 -0600

    ruby string methods

--- ruby/basics/helloWorld.rb
+++ ruby/basics/helloWorld.rb
@@ -1,7 +1,7 @@
 puts "new line"
 print "Hello world"
 print "same line"
-
+puts
 # vars
 testVar = "abc"
 
@@ -11,3 +11,11 @@ numberVar = 123
 decimalVar = 12.3
 boolVar = true
 nullType = nil
+
+stringVar.upcase()
+stringVar.downcase()
+stringVar.length()
+stringVar.strip()
+puts stringVar.include? "t"
+puts stringVar[0, 2]
+puts stringVar.index('s')
