8f7203f0bcd4c406c2d3b849b59881e1a40fed5b
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-08-22 09:42:55 -0600

    ds rb

---
+++ ruby/basics/data_structures.rb
@@ -0,0 +1,20 @@
+# data structures
+arr = Array.new
+arr2 = Array["test"]
+arr.reverse()
+arr.include? "test"
+arr.sort()
+puts arr
+puts arr2
+
+# hash
+hash = {
+    "key" => "val"
+}
+puts hash["key"]
+
+# functions
+def sayhi
+    puts 'hello'
+end
+sayhi()
