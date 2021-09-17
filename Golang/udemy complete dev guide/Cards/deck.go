84d0a6c3df82e8b90b6beff1ea6d9e91474b97ce
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-17 17:13:48 -0600

    shuffle

--- Golang/udemy complete dev guide/Cards/deck.go
+++ Golang/udemy complete dev guide/Cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -47,4 +48,11 @@ func newDeckFromFile(fileName string) deck {
 		os.Exit(1)
 	}
 	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		new_i := rand.Intn(len(d) - 1)
+		d[i], d[new_i] = d[new_i], d[i]
+	}
 }
--- Golang/udemy complete dev guide/Cards/main.go
+++ Golang/udemy complete dev guide/Cards/main.go
@@ -3,6 +3,7 @@ package main
 func main() {
 	// := only for new vars
 	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	hand, remainingDeck := deal(cards, 5)
 	hand.print()
 	remainingDeck.print()
