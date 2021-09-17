d6dd9ccdd55c5c9449772d47d364df02ce3d5405
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-17 13:47:07 -0600

    initialize vars

---
+++ Golang/udemy complete dev guide/Cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c1 string = "five of diamonds"
+	
+	// := only for new vars
+	card := "Ace of Spades"
+	fmt.Println(c1)
+	fmt.Println(card)
+	
+}
