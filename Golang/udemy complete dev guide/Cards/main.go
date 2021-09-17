61136233478791865c1df5808629e64bf681743c
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-17 16:54:34 -0600

    readFile go

--- Golang/udemy complete dev guide/Cards/main.go
+++ Golang/udemy complete dev guide/Cards/main.go
@@ -2,9 +2,8 @@ package main
 
 func main() {
 	// := only for new vars
-	cards := newDeck()
+	cards := newDeckFromFile("my_cards")
 	hand, remainingDeck := deal(cards, 5)
 	hand.print()
 	remainingDeck.print()
-	cards.saveToFile("my_cards")
 }
