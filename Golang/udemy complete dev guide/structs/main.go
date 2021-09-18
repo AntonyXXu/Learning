e1bd57a1c5e6dee0fdd67283cc4b6c02c8e2e85f
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-18 12:18:12 -0600

    structs

--- Golang/udemy complete dev guide/structs/main.go
+++ Golang/udemy complete dev guide/structs/main.go
@@ -2,20 +2,51 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact contactInfo
+}
+
+type personV2 struct {
+	firstName string
+	contactInfo
+	// same as declaring:
+	// contactInfo contactInfo
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newName string) {
+	p.firstName = newName
 }
 
 func main() {
-	p := person{"Antony", "X"}
-	p2 := person{firstName: "Bob", lastName: "X"}
-	fmt.Println(p)
-	fmt.Println(p2)
+	p := person{"Antony", "X", contactInfo{"a@a.ca", 00000}}
+	p2 := person{firstName: "Bob", lastName: "X",
+		contact: contactInfo{
+			email: "b@b.ca",
+			zipCode: 00001,
+		}}
+
+
+	p.print()
+	p2.print()
 
 	var alex person
 	fmt.Printf("%+v", alex)
 	alex.firstName = "alex"
 	alex.lastName = "bob"
 	fmt.Printf("%+v", alex)
+
+	
+
 }
