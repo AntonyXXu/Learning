615c865628e60e02a839e07ac5cafcac8877425d
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-17 18:21:04 -0600

    file test go

--- Golang/udemy complete dev guide/Cards/deck_test.go
+++ Golang/udemy complete dev guide/Cards/deck_test.go
@@ -6,6 +6,6 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) < 4 {
-		t.Errorf("Expected deck of more than 4", len(d))
+		t.Errorf("Expected deck of more than 4 %v", len(d))
 	}
 }
