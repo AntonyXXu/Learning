9b69841a5a51723b6eb8f0adf50fc58c2b9e63cf
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-19 18:35:57 -0600

    writer interface

--- Golang/udemy complete dev guide/interface/http/main.go
+++ Golang/udemy complete dev guide/interface/http/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {}
 
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err!=nil {
 		os.Exit(1)
 	}
-	data := make([]byte, 99999)
-	resp.Body.Read(data)
-	fmt.Println(string(data))
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
 }
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
+} 
