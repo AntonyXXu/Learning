20294157cb12c1f508eddb5b9cb8172cbaaf1b8b
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-21 10:40:27 -0600

    channels

---
+++ Golang/udemy complete dev guide/Channels/web channels/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+	// links are running awaiting previous lines, asynchronous
+	sites := []string{
+		"http://facebook.com",
+		"http://google.com",
+		"http://amazon.com",
+		"http://golang.org",
+	}
+
+	c := make(chan string)
+
+	for _, site := range sites {
+		// only put go in front of function calls
+		go checkLink(site, c)
+	}
+	for {
+		go checkLink(<-c)
+	}
+}
+
+func checkLink(site string, c chan string) bool {
+	_, err := http.Get(site)
+	if err != nil {
+		fmt.Println(site, " offline")
+		c <- site
+		return false
+	}
+
+	fmt.Println(site, " online")
+	c <- site
+	return true
+} 
