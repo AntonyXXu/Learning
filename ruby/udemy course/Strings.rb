204349f3fd647afef41ef1d63fee7c0234bffe0d
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-09-25 19:35:38 -0600

    strings ruby

---
+++ ruby/udemy course/Strings.rb
@@ -0,0 +1,12 @@
+# string interpolation can't be done on single quotes
+a = "fname"
+b = "lname"
+puts a + " " + b
+
+puts "interpolation #{a} t#{b}"
+
+puts a.class
+puts 20.0.class
+puts 10.class
+
+puts a.sub('name', "lalala")
